f973210f11470f7e09c75ea5b150efd59f7cbf19
Kyle Pollina <kylepollina@gmail.com>
2017-02-26 14:25:06 -0500

    Added requirement to get a valid token in account.js

---
+++ routes/account.js
@@ -0,0 +1,16 @@
+var MongoClient = require('mongodb').MongoClient;
+var config      = require('../config'); // get our config file
+var url         = process.env.MONGO_URL || config.database;
+var valid_token = require('./valid_token');
+
+// Expects token
+exports.getAccountInfo = function(req, res) {
+    MongoClient.connect(url, function(err, db) {
+        var colleciton = db.collection('users');
+
+        var userId = req.decoded._id;
+        var userInfo = colleciton.find(ObjectId(userId)).toArray(function(err, document){
+            res.json({firstName: document.firstName, lastName: document.lastName, email: document.email});
+        });
+    });
+}
