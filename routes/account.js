e3913602d5b933ab253aa66b878847d10a6d533d
Kyle Pollina <kylepollina@gmail.com>
2017-02-27 13:50:53 -0500

    Copied encrypt function from auth.js to account.js

--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,9 @@
 var MongoClient = require('mongodb').MongoClient;
 var config      = require('../config'); // get our config file
 var url         = process.env.MONGO_URL || config.database;
+var crypto      = require('crypto');
+var bcrypt      = require('bcrypt');
+var saltRounds  = 10;
 var valid_token = require('./valid_token');
 
 // Expects token
@@ -17,5 +20,16 @@ exports.getAccountInfo = function(req, res) {
 
 // Expects token, firstName, lastName, email, password, new password
 exports.update = function(req, res) {
-    
+    MongoClient.connect(url, function(err, db) {
+        var colleciton = db.collection('users');
+
+        var userId = req.decoded._id;
+        
+    });
+}
+
+function encrypt(password, callback) {
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        callback(err, hash);
+    });
 }
