816c6fe73dd054551b9ecbd679d9b7499c86628b
Keegan <irbyk@purdue.edu>
2017-04-04 16:15:10 -0400

    successfully sends invite emails

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
 	'secret': 'ThIsIsSoSeCrEt',
-	'database': 'mongodb://localhost:27017/turnip'
+	'database': 'mongodb://localhost:27017/turnip',
+	'emailAddress': 'turnipinvites@gmail.com',
+	'emailPassword': 'cs307turnip'
 };
