6aaa72243f847cd6b175e74daf1d27de4c44c92d
Kyle Pollina <kylepollina@gmail.com>
2017-04-06 19:05:01 -0400

    Added script to clean the user collection

--- test/setup.js
+++ test/setup.js
@@ -2,12 +2,19 @@ var MongoClient = require('mongodb').MongoClient;
 var config      = require('../config'); // get our config file
 var url         = process.env.MONGO_URL || config.database;
 
+/* This test run before any other test */
 describe("Setup", function() {
-    it("Cleaning user database", function() {
+    
+    /* DELETES 'users' COLLECTION */
+    /*
+    it("Cleaning user collection...", function() {
         MongoClient.connect(url, function(err, db) {
             db.collection('users', function(err, collection) {
                 collection.remove();
             });
         });
     });
+    */
+
+
 });
