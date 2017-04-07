0fd6850addae67c6d6ce39c000cb0a55b5aee26c
Kyle Pollina <kylepollina@gmail.com>
2017-04-07 02:23:02 -0400

    Added more tests. Also change testing from chai to supertest

--- test/test_server.js
+++ test/test_server.js
@@ -2,32 +2,74 @@ var chai = require('chai');
 var chaiHttp = require('chai-http');
 var server = require('../app');
 var should = chai.should();
-
-chai.use(chaiHttp);
+var supertest = require('supertest');
+var server = supertest.agent("http://localhost:5000");
 
 /**
  * This is where you write your tests. Make sure you follow the structure of the 'Register User' test. 
- * This website helped me writing these tests: http://mherman.org/blog/2015/09/10/testing-node-js-with-mocha-and-chai/
+ * This website helped me writing these tests: https://codeforgeek.com/2015/07/unit-testing-nodejs-application-using-mocha/
  * 
- * To run all test go to the /turnip-api/ folder and run 'npm test'
+ * To run all test go to the /turnip-api/ folder and run 'mocha'
+ * 
+ * Having trouble? Google mocha supertest + your question
  */
 
 
 
 describe('Register User', function() {
     it('should add user TESTING SERVER to the user database on /auth/signup POST', function(done) {
-        chai.request('http://localhost:5000')
-            .post('/auth/signup')
-            .send({'email': 'testing@server.com', 'firstName': 'TESTING', 'lastName': 'SERVER', 'password': 'password'})
-            .end(function(err, res) {
-                res.should.have.status(200);
-                res.should.be.json;
-                res.body.should.be.a('object');
-                res.body.should.have.property('success');
-                res.body.success.should.be.a('boolean');
-                res.body.should.have.property('message');
-                res.body.message.should.be.a('string');
-                done();
-            });
+        server
+        .post("/auth/signup")
+        .send({email: "testing@server.com", firstName: "TESTING", lastName: "SERVER", password: "password"})
+        .expect("Content-type", /json/)
+        .expect(200)
+        .end(function(err, res) {
+            res.status.should.equal(200);
+            res.body.success.should.equal(true);
+            done();
+        });
     });
+
+    it('should not log user in because of a wrong password on /auth/login POST', function(done) {
+        server
+        .post("/auth/login")
+        .send({email: "testing@server.com", password: "notcorrect"})
+        .expect("Content-type", /json/)
+        .expect(200)
+        .end(function(err, res) {
+            res.status.should.equal(200);
+            res.body.success.should.equal(false);
+            done();
+        });
+    });
+
+    it('should not log user in because of a wrong email on /auth/login POST', function(done) {
+        server
+        .post("/auth/login")
+        .send({email: "noemail", password: "password"})
+        .expect("Content-type", /json/)
+        .expect(200)
+        .end(function(err, res) {
+            res.status.should.equal(200);
+            res.body.success.should.equal(false);
+            done();
+        });
+    })
+
+    it('should log user TESTING SERVER in and return success: true and a token on /auth/login POST', function(done) {
+        server
+        .post("/auth/login")
+        .send({email: "testing@server.com", password: "password"})
+        .expect("Content-type", /json/)
+        .expect(200)
+        .end(function(err, res) {
+            res.status.should.equal(200);
+            res.body.success.should.equal(true);
+            res.body.should.have.property("token");
+            done();
+        });
+    });
+
+
 });
+
