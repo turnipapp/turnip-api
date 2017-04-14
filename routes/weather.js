3a57863ef6d9ce0540c5295d9fe5099bb820ca96
kevincardona <kevincardona1@gmail.com>
2017-04-14 15:06:55 -0400

    the weather forecast for today is: sunny

--- routes/weather.js
+++ routes/weather.js
@@ -14,19 +14,20 @@ var lon;
 var get = function (req,res) {
   MongoClient.connect(url, function(err, db) {
     var events = db.collection('events');
+    console.log(req.params);
     events.findOne({_id: new ObjectID(req.params.eventId)}, function (err, event) {
       if (err || !event) {
         res.json({succes: false, message: "Couldn't find event"})
         return;
       } else {
 
+
           var lat = event.location.results[0].geometry.location.lat;
           var lon = event.location.results[0].geometry.location.lng;
           var date = new Date(event.dateStart);
-          console.log(date.getTime());
           var request = unirest.get(base_url + darkskyKey + '/' + lat + ',' + lon + date.getTime()).end(function (response) {
             console.log(response);
-            res.json({success:true, message: response});
+            res.json({success:true, weather: response, location: event.location.results[0].formatted_address});
           })
 
           //console.log(request);
